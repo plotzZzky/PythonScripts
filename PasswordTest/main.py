23cf5969b9e472244106214963030f3759591f4a
Daniel plotzky <daniel.plotzky@gmail.com>
2023-12-06 16:43:28 -0300

    Update main.py

--- PasswordTest/main.py
+++ PasswordTest/main.py
@@ -6,6 +6,7 @@ import time
 import os
 
 
+# Programa para testar a reincidencia de uma senha, e a segurança através de um brute force atack 
 class PasswordTest:
     def __init__(self):
         self.chars = None
@@ -18,12 +19,14 @@ class PasswordTest:
         self.attempts = 0
         self.result = None
 
+    # Tela de boa vinda do programa
     def wellcome(self):
         art.tprint(f'{" " * 23} PasswordTest', 'tarty2')
         print(f"{'-' * 28} Projeto feito por um estudante python {'-' * 26}")
         print(f"{' ' * 34}https://github.com/plotzZzky\n")
         self.menu()
 
+    # Menu principal, para verificar qual a opção o usuario deseja executar
     def menu(self):
         print(f"{'.' * 44} Menu: {'.' * 43}\n"
               "1- BruteForce random password \n"
@@ -43,6 +46,7 @@ class PasswordTest:
             case other:
                 self.menu()
 
+    # Função para determinar os parametros para geração das senhas
     def set_password_chars(self):
         try:
             self.length = int(input('Enter password length: \n'))
@@ -66,22 +70,27 @@ class PasswordTest:
             case _:
                 self.chars = string.digits + string.ascii_letters
 
+    # Gera a senha, com os parametros selecionados pelo usuario
     def generate_pwd(self):
         result = ''.join(random.choice(self.chars) for i in range(self.length))
         return result
 
+    # Função que retorna uma lista com todos os caracteres do tipo de senha selecionada, através dos parametros selecionados pelo usuario
     def get_all_elements(self):
         return (''.join(candidate)
                 for candidate in chain.from_iterable(product(self.chars, repeat=z)
                                                      for z in range(1, self.length + 1)))
-
+        
+    # Inicia o timer para determinar o tempo para quebrar a senha
     def start_timer(self):
         print(f"working...\n")
         self.start_time = time.perf_counter()
 
+    # Encera o timer
     def end_timer(self):
         self.end_time = time.perf_counter() - self.start_time
 
+    # Função que executa o brute force atack
     def bruteforce(self):
         self.attempts = 0
         self.start_timer()
@@ -94,6 +103,7 @@ class PasswordTest:
                     self.show_result()
                     return False
 
+    # Função que testa a reincidencia da senha
     def pwd_test(self):
         self.attempts = 0
         self.start_timer()
@@ -104,6 +114,7 @@ class PasswordTest:
                 self.show_result()
                 return False
 
+    # Verifica o tamanho da senha
     def check_pwd_length(self):
         if len(self.password) == self.length:
             pass
@@ -111,6 +122,7 @@ class PasswordTest:
             print('The password must have the maximum length informed in the script! \n')
             self.menu()
 
+    # Mostra os resultados dos testes
     def show_result(self):
         print(f'{"-" * 42} Eureka!! {"-" * 42}')
         print(f"The result is {self.result}")
@@ -118,18 +130,21 @@ class PasswordTest:
         if __name__ == '__main__':
             self.menu()
 
+    # Função que gera a senha randomica e executa a função do brute force attack
     def test_random_pwd(self):
         self.password = self.generate_pwd()
         print(f"Password = {self.password}\n")
         self.possibilities = self.get_all_elements()
         self.bruteforce()
 
+    # Função que recebe a senha do usuario e executa a função do brute force attack
     def test_your_pwd(self):
         self.password = input('Your password:\n')
         self.check_pwd_length()
         self.possibilities = self.get_all_elements()
         self.bruteforce()
 
+    # Função que recebe a senha do usuario e chama a função que testa a reincidencia
     def test_your_pwd_frequency(self):
         self.password = input('Your password:\n')
         self.check_pwd_length()
