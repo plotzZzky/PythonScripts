58a8d56d8c22fcc2acade732fa9fd164f5d6d0a1
plotzky <dev.plotzky@gmail.com>
2024-07-01 15:35:41 -0300

    Daily update

--- FindCompanyInfo/main.py
+++ FindCompanyInfo/main.py
@@ -1,5 +1,11 @@
+import sys
+from pathlib import Path
+import pandas as pd
+import requests
+import art
+
 """
- Script que automatiza a busca de informações sobre uma empresa no sistema do minha receita:
+Script que automatiza a busca de informações sobre uma empresa no sistema do minha receita:
 
     - Se for fazer muitas consultas, para evitar instabilidade ou sobrecarga no servidor, instale o banco de dados
     na sua maquina e mude a url => https://docs.minhareceita.org/instalacao/
@@ -8,21 +14,52 @@
 
     Obrigado a equipe do minha receita!
 """
-from pathlib import Path
-import pandas as pd
-import requests
-import art
 
 
 class FindInfo:
     """ Busca informações sobre um cnpj no site do minha receita ou no banco de dados local """
+    menu_options = [
+        'Buscar um cnpj',
+        'Buscar um lista de cnpjs em um .csv',
+        'Sair'
+    ]
+
     companies: list = []
-    data: list = []
+    companies_jsons: list = []
 
     def welcome(self):
         art.tprint(f'{" " * 4} FindCompanyInfo', "cybersmall")
         print(f"{'_' * 30} https://github.com/plotzzzky {'_' * 30}\n")
-        self.open_file()
+        self.start_menu()
+
+    def start_menu(self):
+        print(f"{'__' * 20} Menu {'__' * 20}")
+        for index, option in enumerate(self.menu_options, 1):
+            print(f"{index}- {option}")
+
+        option: str = input("Digite a opção: \n")
+        self.check_start_menu_option(option)
+
+    def check_start_menu_option(self, option: str):
+        try:
+            options = [
+                self.get_cnpj,
+                self.open_file,
+                self.exit,
+            ]
+            options[int(option) - 1]()
+        except (IndexError, TypeError, ValueError):
+            print("Opção invalida!\n")
+            self.start_menu()
+
+    def get_cnpj(self):
+        cnpj: str = input("Digite o cnpj sem pontos ou simbolos:\n")
+        cnpj.replace('.', '')
+        cnpj.replace('-', '')
+        cnpj.replace('/', '')
+        company = {'CNPJ': cnpj}
+
+        self.get_data_from_company(company)
 
     def open_file(self):
         """ Abre a lista com todas as empresas """
@@ -32,50 +69,59 @@ class FindInfo:
             dicts: list = df.to_dict(orient='records')
             self.companies: list = [{key: company[key] for key in company} for company in dicts]
 
-            self.find_info()
+            self.select_companies()
         except FileNotFoundError:
-            print("Lista de cnpjs para busca não encontrados!")
+            print("\nLista de cnpjs para busca não encontrados! \n")
+            self.start_menu()
 
-    def find_info(self):
-        """ Busca informações para cada empresa na lista de empresas """
+    def select_companies(self):
+        """ Seleciona cada empresa na lista de empresas e busca informações sobre elas """
         for company in self.companies:
-            self.get_data(company)
-        self.save_file()
+            self.get_data_from_company(company)
+        self.save_result_file()
 
-    def get_data(self, company: dict):
-        """ Busca informações para cada una das empresas da lista """
+    def get_data_from_company(self, company: dict):
+        """ Busca informações da empresa selecionada """
         url: str = f"https://minhareceita.org/{company['CNPJ']}"  # http://0.0.0.0:8000 url local
         response = requests.get(url)
-        json_res: dict = response.json()
 
-        self.format_data(json_res)
+        if response.status_code == 200:
+            json_res: dict = response.json()
+            self.format_response_data(json_res)
+        else:
+            print("\nCNPJ não cadastrado na receita federal!")
 
-    def format_data(self, data: dict):
+    def format_response_data(self, cnpj_data: dict):
         """ Cria um novo json resumido com as informações mais importantes da empresa """
+        endereco = f"{cnpj_data['municipio']}, {cnpj_data['bairro']}, {cnpj_data['logradouro']}, {cnpj_data['numero']}"
+
         data_dict: dict = {
-            "CNPJ": data['cnpj'],
-            "Razao social": data['razao_social'],
-            "nome fantasia": data['nome_fantasia'],
-            "email": data['email'],
-            "telefone": data['ddd_telefone_1'],
-            "telefone_2": data['ddd_telefone_2'],
-            "ativa": data['descricao_situacao_cadastral'],
-            "mei": data['opcao_pelo_mei'],
-            "simples": data['opcao_pelo_simples'],
-            "endereco": f"{data['municipio']}, {data['bairro']}, {data['logradouro']}, {data['numero']}",
+            "CNPJ": cnpj_data['cnpj'],
+            "Razao social": cnpj_data['razao_social'],
+            "nome fantasia": cnpj_data['nome_fantasia'],
+            "email": cnpj_data['email'],
+            "telefone": cnpj_data['ddd_telefone_1'],
+            "telefone_2": cnpj_data['ddd_telefone_2'],
+            "ativa": cnpj_data['descricao_situacao_cadastral'],
+            "mei": cnpj_data['opcao_pelo_mei'],
+            "simples": cnpj_data['opcao_pelo_simples'],
+            "endereco": endereco,
         }
 
-        self.data.append(data_dict)
+        self.companies_jsons.append(data_dict)
 
-    def save_file(self):
+    def save_result_file(self):
         """ Salva uma nova planilha (info.ods) com as informações """
-        new_df = pd.DataFrame(self.data)
+        new_df = pd.DataFrame(self.companies_jsons)
         new_df.to_excel('info.ods', index=False)
-        self.exit()
+        print('Script completado e arquivo info.ods gerado!')
 
     @staticmethod
     def exit():
-        print('Script completado e arquivo info.ods gerado!')
+        print("Saindo...")
+
+        if __name__ == '__main__':
+            sys.exit()
 
 
 find = FindInfo()
