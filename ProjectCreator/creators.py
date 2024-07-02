f7a8a61f1cb7bb26bd68cd654b29f2a669590696
plotzky <dev.plotzky@gmail.com>
2024-07-02 15:31:39 -0300

    Added projectCreator

---
+++ ProjectCreator/creators.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+import os
+
+
+class CreatorBase:
+    """ Classe base do criador dos projetos com as funções genericas """
+    project_name: str = ""
+
+    # Generics functions
+    @staticmethod
+    def get_project_name() -> str:
+        option: str = input("Digite o nome do projeto: \n")
+        return option
+
+    def create_project_folder(self) -> bool:
+        folder: bool = Path(self.project_name).exists()
+        if not folder:
+            Path(self.project_name).mkdir()
+            return True
+        else:
+            return False
+
+    def create_readme(self):
+        file_path: str = f"{self.project_name}/readme.md"
+        content: str = f"### {self.project_name}"
+
+        with open(file_path, 'w') as file:
+            file.write(content)
+
+    @staticmethod
+    def get_requirements():
+        options: str = input("Digite os requirements do projeto separados apenas com espaços: \n")
+        return options
+
+    def create_generic_contents(self):
+        self.project_name: str = self.get_project_name()
+        self.create_project_folder()
+        self.create_readme()
+
+
+class PythonCreator(CreatorBase):
+    """ Classe para criar a estrutura do projeto python"""
+    def create_python_project(self):
+        self.create_generic_contents()
+        self.create_main_python_file()
+        self.create_python_requirements()
+        self.create_venv()
+
+    def create_python_requirements(self):
+        options: str = self.get_requirements()
+        file_path: str = f"{self.project_name}/requirements.txt"
+
+        with open(file_path, 'w') as file:
+            for item in options.split(" "):
+                file.write(item)
+
+    def create_venv(self):
+        if os.name == 'nt':
+            cmd = "venv/Scripts/activate.bat"
+        else:
+            cmd = "source venv/bin/activate"
+
+        os.system(f"cd {self.project_name}; python -m venv venv; {cmd}; pip install -r requirements.txt")
+
+    def create_main_python_file(self):
+        file_path: str = f"{self.project_name}/xx.py"
+        with open(file_path, 'w') as file:
+            file.write("# new project")
+
+
+class JsCreator(CreatorBase):
+    """ Classe para criar a estrutura do projeto js """
+    def create_js_project(self):
+        self.create_generic_contents()
+        self.create_main_js_file()
+        self.create_npm_folder()
+        self.install_js_requirements()
+
+    def create_main_js_file(self):
+        file_path: str = f"{self.project_name}/main.js"
+        with open(file_path, 'w') as file:
+            file.write("// new project")
+
+    def create_npm_folder(self):
+        os.system(f"cd {self.project_name}; npm init")
+
+    def install_js_requirements(self):
+        options: str = self.get_requirements()
+
+        for item in options.split(" "):
+            os.system(f"cd {self.project_name}; npm install {item}")
---
+++ ProjectCreator/main.py
@@ -0,0 +1,80 @@
+from creators import PythonCreator, JsCreator
+import sys
+
+
+class MenuClass:
+    """ Classe do Menu inicial """
+    APP_NAME: str = 'Menu'
+    APP_DESC: str = "Ferramenta para criar projetos base em python ou javascript"
+
+    project_name: str = ""
+
+    def __init__(self):
+        # O init é necessario para usar o self
+        self.menu_options = [
+            self.create_python_project,
+            self.create_js_project,
+            lambda: self.check_exit_menu_option('Y'),  # Chama a função para fechar o app
+        ]
+
+    def welcome(self):
+        print(f"{' ' * 35} {self.APP_NAME}")
+        print(self.APP_DESC)
+        self.start_menu()
+
+    def start_menu(self):
+        print(f"{'--' * 18} Menu {'--' * 18}")
+        options = [
+            'Criar projeto Python',
+            'Criar projeto Js',
+            'Sair'
+        ]
+
+        for index, option in enumerate(options, 1):
+            print(f"{index}- {option}")
+
+        self.check_start_menu_option()
+
+    def check_start_menu_option(self):
+        try:
+            option: int = int(input("\nSelecione uma opção:\n")) - 1
+            self.menu_options[option]()  # chama a função selecionada
+
+        except (ValueError, IndexError):
+            print('\nOpção invalida!!!!!\n')
+            self.start_menu()
+        except KeyboardInterrupt:
+            print("\nSaindo...")
+
+    def create_python_project(self):
+        creator_py.create_python_project()
+        print("Projeto Python criado!")
+        self.exit_menu()
+
+    def create_js_project(self):
+        creator_js.create_js_project()
+        print("Projeto JavaScript criado!")
+        self.exit_menu()
+
+    def exit_menu(self):
+        try:
+            option: str = input("\nSair?(Y/N)\n").upper()
+            self.check_exit_menu_option(option)
+        except KeyboardInterrupt:
+            sys.exit()
+
+    def check_exit_menu_option(self, option: str):
+        if option == 'Y':
+            if __name__ == '__main__':
+                print("Saindo...")
+                sys.exit()
+        else:
+            self.start_menu()
+
+
+creator_py = PythonCreator()
+creator_js = JsCreator()
+menu = MenuClass()
+
+if __name__ == '__main__':
+    menu.welcome()
